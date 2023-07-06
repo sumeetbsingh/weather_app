f05d9b29d048d5e73201f7660a3e3e971c0444e5
Sumeet Birendra Singh <sumeetbsingh2002@gmail.com>
2023-07-06 22:17:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,56 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useEffect, useState}from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import * as Location from 'expo-location';
+
+import DateTime from './components/DateTime'
+import WeatherScroll from './components/WeatherScroll'
+const API_KEY ='c19fa19ef11b4b6afb77be7c89d883b0';
+const img = require('./assets/image.png')
+export default function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        fetchDataFromApi("40.7128", "-74.0060")
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      fetchDataFromApi(location.coords.latitude, location.coords.longitude);
+    })();
+  }, [])
+
+  const fetchDataFromApi = (latitude, longitude) => {
+    if(latitude && longitude) {
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
+
+      // console.log(data)
+      setData(data)
+      })
+    }
+    
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={img} style={styles.image} >
+        <DateTime current={data.current} timezone={data.timezone} lat={data.lat} lon={data.lon}/>
+        <WeatherScroll weatherData={data.daily}/>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image:{
+    flex:1, 
+    resizeMode:"cover", 
+    justifyContent:"center"
+  }
+});
